e9d4ca0b1b2d5917ab016443de394ef644796fcd
ferdisahin <ferdisahin@hotmail.fr>
2022-02-17 15:21:14 +0300

    404.php & style.css changed!

--- 404.php
+++ 404.php
@@ -19,7 +19,6 @@ get_header();
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'eternal' ); ?></p>
-
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 		</div>
